620769252a9030025cb5ce86a1e8f7ebd2250253
Rakesh20j <Rakesh.a07@gmail.com>
2018-10-13 22:21:00 -0400

    hello program

---
+++ hello/IfElse.py
@@ -0,0 +1,8 @@
+a=1
+b=1
+if a<b:
+    print("a is less thean b")
+elif a==b:
+    print("a is equal to b")
+else:
+    print("b is greater than a")
---
+++ hello/function.py
@@ -0,0 +1,13 @@
+#comments
+def function1():
+    print("ahh")
+    print("0000000ahhh2")
+print("outside function")
+
+
+
+def function2(x):
+    return 2*x
+
+function2(3)
+function1()
